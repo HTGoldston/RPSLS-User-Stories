55dab6311098c9ab7675dc2c249b45bd0e9d55f3
HTGoldston <83466268+HTGoldston@users.noreply.github.com>
2021-05-17 14:10:01 -0400

    v1.2 created last Object, start methods
    Created Class other and started on  methods:
    2.1 game_play
    2.2 player_values
    2.3 outcome

---
+++ game.py
@@ -0,0 +1,24 @@
+class Game
+    def __init__(self):
+        self.game= Game
+
+        #
+        def play():
+            pass
+        #user?
+        #ai
+        #other
+
+        def choices()
+        #r
+        #s
+        #s
+            pass
+
+        def randon?
+
+        #choice(range(start, stop, step))
+            pass
+
+#   super()init
+        #some_thing = both human_n_cpu
--- human.py
+++ human.py
@@ -1,5 +1,6 @@
 class Human
-    def __init__(self)
+    def __init__(self):
         self.human = Human
-        def play():
-            user =input()
+
+
+
